600e49f2138ab75c3273e12c83da5bd7757a0c05
jacekkulma <jacek.kulma@outlook.com>
2025-06-06 11:33:51 +0200

    model vs model func & demo script

--- compare_models.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from connect4_model_vs_model import play_game
-from utils import minimax
-
-# example
-if __name__ == "__main__":
-    play_game(minimax, minimax)
--- connect4_model_vs_model.py
+++ connect4_model_vs_model.py
@@ -1,60 +1,93 @@
 import numpy as np
 import random
 import math
-from utils import create_board, drop_piece, is_valid_location, get_next_open_row, print_board, winning_move
+import pygame
+from utils import create_board, drop_piece, is_valid_location, get_next_open_row, print_board, winning_move, create_screen, draw_board
 
 
 # returns [AI1_moves, AI2_moves, winner]
-def play_game(model1, model2):
+def play_game(model1, model2, console_output=True, pygame_window=False, red=(255,0,0), yellow=(255,255,0)):
     
     AI1_moves = 0
     AI2_moves = 0
 
-    PLAYER = 0
-    AI = 1
+    AI1 = 0
+    AI2 = 1
 
-    PLAYER_PIECE = 1
-    AI_PIECE = 2
+    EMPTY = 0
+    AI1_PIECE = 1
+    AI2_PIECE = 2
 
 
     board = create_board()
-    print_board(board)
+
+    if console_output:
+        print_board(board)
+
     game_over = False
 
-    turn = random.randint(PLAYER, AI)
+    if pygame_window:
+        pygame.init()
+        screen = create_screen()
+        draw_board(board, AI1_PIECE, AI2_PIECE, screen)
+        pygame.display.update()
+        myfont = pygame.font.SysFont("monospace", 75)
+
+    turn = random.randint(AI1, AI2)
         
     while not game_over:
         # AI1 turn
-        if turn == PLAYER and not game_over:
-            col, model_score = model1(board, 5, -math.inf, math.inf, True)
+        if turn == AI1 and not game_over:
+            col, model_score = model1(board, 5, -math.inf, math.inf, True, AI1_PIECE, AI2_PIECE, EMPTY)
             col = int(col)
 
             if is_valid_location(board, col):
                 AI1_moves += 1
                 row = get_next_open_row(board, col)
-                drop_piece(board, row, col, PLAYER_PIECE)
+                drop_piece(board, row, col, AI1_PIECE)
 
-                if winning_move(board, PLAYER_PIECE):
+                if winning_move(board, AI1_PIECE):
+                    if pygame_window:
+                        label = myfont.render("Player 1 wins!!", 1, red)
+                        screen.blit(label, (40,10))
                     game_over = True
-                    return [AI1_moves, AI2_moves, model1.__name__]
-        
-        if turn == AI and not game_over:
-            col, model_score = model2(board, 5, -math.inf, math.inf, True)
-            col = int(col)
-
-            if is_valid_location(board, col):
-                AI1_moves += 1
-                row = get_next_open_row(board, col)
-                drop_piece(board, row, col, AI_PIECE)
-
-                if winning_move(board, AI_PIECE):
-                    game_over = True
-                    return [AI1_moves, AI2_moves, model2.__name__]
+                    winner = "AI1"
+                    winner_alg = model1.__name__
                 
-                print_board(board)
+                if console_output:
+                    print_board(board)
+                if pygame_window:
+                    draw_board(board, AI1_PIECE, AI2_PIECE, screen)
+
+                turn += 1
+                turn = turn % 2
+        
+        if turn == AI2 and not game_over:
+            col, model_score = model2(board, 5, -math.inf, math.inf, True, AI1_PIECE, AI2_PIECE, EMPTY)
+            col = int(col)
+
+            if is_valid_location(board, col):
+                AI1_moves += 1
+                row = get_next_open_row(board, col)
+                drop_piece(board, row, col, AI2_PIECE)
+
+                if winning_move(board, AI2_PIECE):
+                    if pygame_window:
+                        label = myfont.render("Player 2 wins!!", 1, yellow)
+                        screen.blit(label, (40,10))
+                    game_over = True
+                    winner = "AI2"
+                    winner_alg = model2.__name__
+                
+                if console_output:
+                    print_board(board)
+                if pygame_window:
+                    draw_board(board, AI1_PIECE, AI2_PIECE, screen)
 
                 turn += 1
                 turn = turn % 2
         
         if game_over:
-            return
+            if pygame_window:
+                pygame.time.wait(3000)
+            return [AI1_moves, AI2_moves, f"{winner}: {winner_alg}"]
