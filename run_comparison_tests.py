bb5dce7c83d1d201e4f4af6236dcf8dc613146f9
jacekkulma <jacek.kulma@outlook.com>
2025-06-06 16:15:11 +0200

    save by display name not func name

--- run_comparison_tests.py
+++ run_comparison_tests.py
@@ -11,7 +11,14 @@ import csv
 output_folder = "tests"
 os.makedirs(output_folder, exist_ok=True)
 
-models = [get_random_move, get_mcts_move]
+models = {
+    "Random": get_random_move,
+    "Base MCTS": get_mcts_move
+}
+model_names = {v: k for k, v in models.items()}
+func_name_to_model_name = {v.__name__: k for k, v in models.items()}
+
+# models = [get_random_move, get_mcts_move]
 # seeds = [42, 1234, 987654, 20240406, 777, 314159, 8675309, 99999, 1337, 55555]
 # seeds = [42, 1234]
 seeds = [
@@ -21,11 +28,12 @@ seeds = [
 ]
 
 def main(skip_self_comparison=True) -> None:
+    model_funcs = list(models.values())
     # Skip comparing the model with itself, if the flag is set
     if skip_self_comparison:
-        models_to_compare = combinations(models, 2)
+        models_to_compare = combinations(model_funcs, 2)
     else:
-        models_to_compare = combinations_with_replacement(models, 2)
+        models_to_compare = combinations_with_replacement(model_funcs, 2)
 
     # print(list(models_to_compare))
     # import sys
@@ -34,18 +42,26 @@ def main(skip_self_comparison=True) -> None:
         # Count wins and turns for each model inside seeds loop
         win_counter = defaultdict(int)
         turn_counter = defaultdict(list)
+
         for seed in seeds:
             random.seed(seed)
             ai1_moves, ai2_moves, winner, winner_alg = play_game(model1, model2, console_output=False, pygame_window=False)
-            win_counter[winner_alg] += 1
-            turn_counter[winner_alg].append(max(ai1_moves, ai2_moves))
+            # win_counter[winner_alg] += 1
+            # turn_counter[winner_alg].append(max(ai1_moves, ai2_moves))
+            if winner_alg in func_name_to_model_name:
+                name = func_name_to_model_name[winner_alg]
+                win_counter[name] += 1
+                turn_counter[name].append(max(ai1_moves, ai2_moves))
+
+        name1 = model_names[model1]
+        name2 = model_names[model2]
     
-        file_name = f"{model1.__name__}-vs-{model2.__name__}.csv"
+        file_name = f"{name1.replace(' ', '_')}_vs_{name2.replace(' ', '_')}.csv"
         with open(os.path.join(output_folder, file_name), "w", newline="") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(["name", "wins", "avg_turns", "turns"])
             for model in [model1, model2]:
-                name = model.__name__
+                name = model_names[model]
                 wins = win_counter.get(name, 0)
                 turns = turn_counter.get(name, [])
                 avg_turns = sum(turns) / len(turns) if turns else 0
