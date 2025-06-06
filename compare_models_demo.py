7689cc5156741108e129135afb4503e95d7fb0fa
jacekkulma <jacek.kulma@outlook.com>
2025-06-06 13:50:01 +0200

    model vs nodel using mcts

--- compare_models_demo.py
+++ compare_models_demo.py
@@ -1,8 +1,8 @@
 import numpy as np
 from connect4_model_vs_model import play_game
-from utils import minimax
+from mcts_interface import get_mcts_move
 
 # example
 if __name__ == "__main__":
-    ai1_moves, ai2_moves, winner = play_game(minimax, minimax, console_output=True, pygame_window=True)
+    ai1_moves, ai2_moves, winner = play_game(get_mcts_move, get_mcts_move, console_output=True, pygame_window=True)
     print(f"winner: {winner}, moves: {max(ai1_moves, ai2_moves)}")
