40a2a2bcfd7b1024665f97d7481f89b6b9507931
jacekkulma <jacek.kulma@outlook.com>
2025-06-06 15:12:44 +0200

    change return list from play_game

--- compare_models_demo.py
+++ compare_models_demo.py
@@ -5,5 +5,5 @@ from utils import get_random_move
 
 # example
 if __name__ == "__main__":
-    ai1_moves, ai2_moves, winner = play_game(get_mcts_move, get_random_move, console_output=True, pygame_window=True)
-    print(f"winner: {winner}, moves: {max(ai1_moves, ai2_moves)}")
+    ai1_moves, ai2_moves, winner, winner_alg = play_game(get_mcts_move, get_random_move, console_output=True, pygame_window=True)
+    print(f"winner: {winner} - {winner_alg}, moves: {max(ai1_moves, ai2_moves)}")
